304a789056f57ce86f8c156ac123f91e34267905
Sunny Prakash <sunny.prakashgm@gmail.com>
2020-07-19 19:36:03 +0530

    tweaked some configurations

--- server/config/db.ts
+++ server/config/db.ts
@@ -4,8 +4,13 @@ import config from 'config';
 const uri: string = config.get('mongoURI');
 
 const connectDB = () => {
-    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-        .then(() => console.log('Connected to DB'))
+    mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        poolSize: 10,
+        bufferMaxEntries: 0
+    }).then(() => console.log('Connected to DB'))
         .catch(err => console.log('DB connection error: ', err))
 }
 
