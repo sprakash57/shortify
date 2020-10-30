5a24ab2ef124668f98a54a6cfa479904508bfd22
pitchayut-junior-tw <pitchayut.c@thoughtworks.com>
2020-10-30 10:11:50 +0700

    Add types to variables & functions

--- client/src/components/Home/Home.ts
+++ client/src/components/Home/Home.ts
@@ -2,39 +2,50 @@ import Vue from 'vue';
 import List from '../List/List.vue';
 import axios from 'axios';
 import { API_URL, URL_PATTERN } from '@/constants';
+import { WebUrl } from '@/types/web-url';
+import { Mode } from '@/types/theme';
+import { WebUrlResponse } from '@/types/api'
+
+type Data = {
+    newUrl: string;
+    urls: WebUrl[];
+    errorMessage: string;
+    warningMessage: string;
+    mode: Mode;
+}
 
 export default Vue.extend({
     name: 'Home',
     components: {
         List
     },
-    data() {
+    data(): Data {
         return {
             newUrl: '',
-            urls: [] as any[],
+            urls: [],
             errorMessage: '',
             warningMessage: '',
-            mode: 'light'
+            mode: Mode.LIGHT
         }
     },
     watch: {
         mode: function (val) {
           this.$emit('changeMode',val)
           const body = document.getElementsByTagName('body')[0]
-          val === 'dark' ? body.style.backgroundColor = 'black' : body.style.backgroundColor = 'white'
+          val === Mode.DARK ? body.style.backgroundColor = 'black' : body.style.backgroundColor = 'white'
         }
     },
     computed: {
         iconType(): string {
-            return (this.mode === 'dark') ? 'sun' : 'moon';
+            return (this.mode === Mode.DARK) ? 'sun' : 'moon';
         },
         iconClass(): string {
-            return (this.mode === 'dark') ? 'icon--yellow' : 'icon--grey';
+            return (this.mode === Mode.DARK) ? 'icon--yellow' : 'icon--grey';
         },
     },
     methods: {
         setMode() {
-            const setter = (this.mode === 'dark') ? 'light' : 'dark'
+            const setter = (this.mode === Mode.DARK) ? Mode.LIGHT : Mode.DARK
             this.mode = setter
         },
         postUrl() {
@@ -42,12 +53,12 @@ export default Vue.extend({
             console.log(this.warningMessage);
             if (this.warningMessage === '') {
                 axios
-                    .post(`${API_URL}/create`, { inputUrl: this.newUrl })
+                    .post<WebUrlResponse>(`${API_URL}/create`, { inputUrl: this.newUrl })
                     .then(response => {
                         if (!response.data.error) this.urls.unshift(response.data);
                         else this.errorMessage = response.data.error;
                     })
-                    .catch(error => this.errorMessage = 'You might wanna check your network instead!! 😫');
+                    .catch(() => this.errorMessage = 'You might wanna check your network instead!! 😫');
             }
             this.newUrl = '';
         },
@@ -59,9 +70,9 @@ export default Vue.extend({
     },
     mounted() {
         axios
-            .get(API_URL)
+            .get<WebUrlResponse[]>(API_URL)
             .then(response => this.urls = response.data)
-            .catch(error => this.errorMessage = 'hmm... It seems network is broken or server is down 😴');
-        this.mode = localStorage.getItem('mode') || 'light'
+            .catch(() => this.errorMessage = 'hmm... It seems network is broken or server is down 😴');
+        this.mode = localStorage.getItem('mode') as Mode || Mode.LIGHT
     }
 })
